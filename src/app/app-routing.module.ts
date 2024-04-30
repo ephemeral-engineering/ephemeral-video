6d0cc16ba8485474b7a5b89868fa117c9dc02b59
akigrafsoft <akigrafsoft@gmail.com>
2024-04-30 21:15:51 +0200

    code cleanup: remove login and bye

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ByeComponent } from './bye/bye.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 
-import { APP_BASE_HREF } from '@angular/common';
+// import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, //, canActivate: [AuthGuard]
   { path: ':id', component: HomeComponent },
-  { path: 'bye', component: ByeComponent },
-  { path: 'login', component: LoginComponent },
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
   // { path: ':id', redirectTo: '/home/:id', pathMatch: 'full' }
 ];
